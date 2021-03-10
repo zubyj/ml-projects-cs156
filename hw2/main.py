28f86439354c39e8c6e267c41f0d6f827445aaab
zuby <zubyjaved@gmail.com>
2021-03-10 14:59:39 -0800

    predict house prices for some test cases

--- hw2/main.py
+++ hw2/main.py
@@ -7,15 +7,19 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics._regression import mean_squared_error
+from numpy.lib.scimath import sqrt
+from test_set import test_set
 
 # Read dataset of houses & use house price as predictive output (dependent variable)
 # Every other housing quality is an indepedent variable. 
 dataset = pd.read_csv('Housing-Data-one-zip-4.csv')
-print(dataset)
 x = dataset.iloc[: , dataset.columns != 'price'].values
 y = dataset.iloc[: , -2].values
 
@@ -25,9 +29,29 @@ x = np.array(ct.fit_transform(x))
 
 # Transform year built to age of building 
 x[: , -1] = 2021 - x[: , -1]
-print(x[: , -1])
 
+# Simple Linear Regression Model
+#
+# Train using sqft_living as independent variable
 # Divide into training & test set
-sqft_living = x[: , -9]
+sqft_living = x[: , 17:18]
 x_train, x_test, y_train, y_test = train_test_split(sqft_living, y, test_size=.2, random_state=1)
-print(x_train)
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+y_pred = regressor.predict(x_test)
+# Show root mean squared value (R squared)
+rms = sqrt(mean_squared_error(y_test, y_pred))
+print("RMS " + str(rms))
+# Plot linear regression for training data
+plt.scatter(x_train, y_train, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.show()
+# Plot linear regression for test data
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_test, y_pred, color='blue')
+plt.show()
+# Predict house prices for some test cases
+sqft_living_test = test_set[: , 2:3]
+for sqft_living in sqft_living_test:
+    print("SQFT LIVING : " + str(sqft_living))
+    print("PRED PRICE " + str(regressor.predict([sqft_living])))
